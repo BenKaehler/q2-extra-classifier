9ecaaf2a20445b845748033f64debbd3b1aa1886
Ben Kaehler <kaehler@gmail.com>
2017-05-29 22:48:28 +1000

    FIX: added ChunkedHashingVectorizer and fixed norm snafu

--- q2_extra_classifier/_extra.py
+++ q2_extra_classifier/_extra.py
@@ -1,8 +1,9 @@
 from q2_feature_classifier.classifier import _register_fitter
 
 nb_extra = [['feat_ext',
-             {'__type__': 'feature_extraction.text.HashingVectorizer',
+             {'__type__': 'custom.ChunkedHashingVectorizer',
               'analyzer': 'char',
+              'norm': None,
               'non_negative': True}],
             ['norm',
              {'__type__': 'feature_extraction.text.TfidfTransformer'}],
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="q2-extra-classifier",
-    version='2017.3.0.dev',
+    version='2017.5.0.dev',
     packages=find_packages(),
     install_requires=['q2-feature-classifier', 'q2cli'],
     author="Ben Kaehler",
